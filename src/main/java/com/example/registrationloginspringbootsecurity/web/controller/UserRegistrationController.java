fba6a291f286fed5a2121d6363ded4b0c1654051
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-09 10:14:14 +0100

    minor refactor in controller class

--- src/main/java/com/example/registrationloginspringbootsecurity/web/controller/UserRegistrationController.java
+++ src/main/java/com/example/registrationloginspringbootsecurity/web/controller/UserRegistrationController.java
@@ -2,22 +2,21 @@ package com.example.registrationloginspringbootsecurity.web.controller;
 
 import com.example.registrationloginspringbootsecurity.service.UserService;
 import com.example.registrationloginspringbootsecurity.web.dto.UserRegistrationDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/registration")
 public class UserRegistrationController {
 
     private final UserService userService;
-
-    public UserRegistrationController(UserService userService) {
-        this.userService = userService;
-    }
 
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
         userService.save(registrationDto);
         return "redirect:/registration?success";
     }
 }
+
