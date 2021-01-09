28e53b1454db857a8a5a7f505aa9643d917575b9
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-09 09:02:30 +0100

    created project structure and connection to database

---
+++ src/main/java/com/example/registrationloginspringbootsecurity/RegistrationLoginSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginspringbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registrationloginspringbootsecurity/RegistrationLoginSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginspringbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
