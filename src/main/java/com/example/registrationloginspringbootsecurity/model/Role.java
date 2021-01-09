794a27d86c4ce4fa16f68c7e7e89dbe14f984780
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-09 09:20:05 +0100

    created Role class with addontations

--- src/main/java/com/example/registrationloginspringbootsecurity/model/Role.java
+++ src/main/java/com/example/registrationloginspringbootsecurity/model/Role.java
@@ -1,5 +1,22 @@
 package com.example.registrationloginspringbootsecurity.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
