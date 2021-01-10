ef69e894090417e3bfcd007ec22152dfa273f09c
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-10 16:34:07 +0100

    created showRegistrationForm in controller and added registatrion.html

--- src/main/java/com/example/registrationloginspringbootsecurity/web/controller/UserRegistrationController.java
+++ src/main/java/com/example/registrationloginspringbootsecurity/web/controller/UserRegistrationController.java
@@ -4,7 +4,9 @@ import com.example.registrationloginspringbootsecurity.service.UserService;
 import com.example.registrationloginspringbootsecurity.web.dto.UserRegistrationDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +16,12 @@ public class UserRegistrationController {
 
     private final UserService userService;
 
+    @GetMapping
+    private String showRegistrationForm(){
+        return "registration";
+    }
+
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
         userService.save(registrationDto);
         return "redirect:/registration?success";
