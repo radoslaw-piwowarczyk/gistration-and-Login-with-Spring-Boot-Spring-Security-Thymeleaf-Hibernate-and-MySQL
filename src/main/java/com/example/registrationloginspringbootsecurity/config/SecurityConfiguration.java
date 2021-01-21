36cbcb9f8ef0e185f6a0abfbc58ba840da7fd51e
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-21 18:12:21 +0100

    added DaoAuthenticactionProvider

--- src/main/java/com/example/registrationloginspringbootsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/example/registrationloginspringbootsecurity/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.registrationloginspringbootsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,8 +14,16 @@ import org.springframework.security.web.util.matcher.AndRequestMatcher;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
     }
 
     @Override
