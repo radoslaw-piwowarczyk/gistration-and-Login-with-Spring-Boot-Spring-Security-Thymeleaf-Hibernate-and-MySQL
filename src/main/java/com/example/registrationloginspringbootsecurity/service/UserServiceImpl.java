5475261266b53d3261e70854c715d2c6c9dcee17
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-21 18:34:51 +0100

    added implementation of loadUserByUsername

--- src/main/java/com/example/registrationloginspringbootsecurity/service/UserServiceImpl.java
+++ src/main/java/com/example/registrationloginspringbootsecurity/service/UserServiceImpl.java
@@ -32,7 +32,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("Invalud username or password");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), null);
     }
 }
