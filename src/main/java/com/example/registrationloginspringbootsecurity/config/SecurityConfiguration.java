3fea054425250ace764550e3e37ab734a40b4ec7
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-21 18:09:42 +0100

    added passwordEncoder

--- src/main/java/com/example/registrationloginspringbootsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/example/registrationloginspringbootsecurity/config/SecurityConfiguration.java
@@ -1,14 +1,22 @@
 package com.example.registrationloginspringbootsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
